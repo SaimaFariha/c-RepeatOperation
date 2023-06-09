3d5b2b7756c80d4b91e840eaa8ee6dedf5cba75a
SAIMAALI\LENOVO <saima.miti7@gmail.com>
2023-06-09 15:03:56 +0600

    Initial Commit

---
+++ C#BasicOOP/C#BasicOOP/Clar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_BasicOOP
+{
+    public enum Color 
+    {
+        Red,
+        Green,
+        Blue,
+        Black,
+    }
+    internal class car
+    {
+        public Color color;
+        public string name;
+        public string brand;
+        public DateTime year;
+
+        public static int price;
+
+        public void Start()
+        { 
+            Console.WriteLine("car has started");
+        }
+        public void Speed()
+        {
+            Console.WriteLine("Car speed is 180 kmph");
+        }
+    }
+}
---
+++ C#BasicOOP/C#BasicOOP/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace C_BasicOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            car bmw = new car();
+            bmw.Start();
+            bmw.color = Color.Red;
+            Console.WriteLine(bmw.color);
+            bmw.Speed();
+
+            car audi = new car();
+            audi.Start();
+            audi.Speed();
+        }
+    }
+}
